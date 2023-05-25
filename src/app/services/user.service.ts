ff2d23d7d99422054b3a4da19bcc034cf9a879f5
ManojPrasannakumar <60639787+ManojPrasannakumar@users.noreply.github.com>
2023-05-25 17:48:27 +0530

    1 binaries uploaded

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,42 +4,39 @@ export interface userType {
   empName: string;
   position?: number;
   empGender: string;
-  empTech: string;
+  empTech:string;
 }
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class UserService {
-  userData: userType[] = [
-    {
-      empName: 'name',
-      empGender: 'male',
-      empTech: 'tech',
-      position: 1,
-    },
-  ];
+  userData:userType[]=[{
+    empName: 'name',
+    empGender: 'male',
+    empTech:'tech',
+    position:1
+  }]
 
-  loginStatus = false;
+  loginStatus=false;
   loginEvent = new EventEmitter();
 
-  constructor() {}
+  constructor() { }
 
-  newUserData = (userObj: userType) => {
-    const newUser = { ...userObj, position: this.userData.length + 1 };
+  newUserData = (userObj:userType)=>{
+    const newUser = {...userObj,position:this.userData.length+1};
     this.userData.push(newUser);
-  };
+  }
 
-  updateUserData = (userObj: userType) => {
-    const userIndex = this.userData.findIndex(
-      (user) => user.position === userObj.position
-    );
-    console.log('userIndex', userIndex);
-    this.userData[userIndex] = userObj;
-  };
+  updateUserData = (userObj:userType)=>{
+    const userIndex=this.userData.findIndex(user=>user.position === userObj.position);
+    console.log('userIndex',userIndex);
+    this.userData[userIndex]= userObj;
+  }
 
-  changeLoginStatus(status: boolean) {
-    this.loginStatus = status;
+  changeLoginStatus(status:boolean){
+    this.loginStatus= status;
     this.loginEvent.emit();
   }
+
 }
